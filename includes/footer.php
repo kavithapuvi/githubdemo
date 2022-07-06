0d15beca27d906c8de9ff566bb5211318e523a7b
kavithapuvi <108470990+kavithapuvi@users.noreply.github.com>
2022-07-06 15:21:22 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+    <footer>
+        <div class="container"> 
+            <div class="footer flex">
+                <ul class="Flinks flex">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="products.php">Products</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+                <ul class="Fsocial flex my-4">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+                <ul class="Faddress">
+                    <li class="w-75 mx-auto">No. 60, Salavaiyar Colony, 2nd Street, Saidapet panagal maligai building, Chennai - 600015, Tamil Nadu, India</li>
+                    <li>+91 80486 06366</li>
+                    <li>info@ysrenterprises.com</li>
+                </ul>
+            </div>
+        </div>
+        <p class="copyright">All rights reserved 2022 <i class="fa fa-copyright"></i> YSR Enterprises</p>
+    </footer>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.pogo-slider.min.js"></script>
+    <script src="js/slick.js"></script>
+    <script src="js/script.js"></script>
---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>YRS Enterprises</title>
+        <link rel="icon" href="images/logo.png">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/pogo-slider.min.css">
+        <link rel="stylesheet" href="css/slick.css">
+        <link rel="stylesheet" href="css/slick-theme.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+        <a type="button" id="topscroll"><i class="fa fa-angle-double-up"></i></a>
+        <header class="header">
+            <nav class="navbar navbar-expand-lg">
+                <a href="index.php" class="navbar-brand">
+                    <img src="images/logo.png" alt="" class="objectContain">
+                    <h5>YRS Enterprises</h5>
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#collapsibleNavbar">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                    <ul class="navbar-nav justify-content-center">
+                        <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                        <li class="nav-item"><a href="about.php" class="nav-link">About us</a></li>
+                        <li class="nav-item"><a href="products.php" class="nav-link">Products</a></li>
+                        <li class="nav-item"><a href="client.php" class="nav-link">Clients</a></li>
+                        <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+                    </ul>
+                    <ul class="sociallist flex">
+                        <li><a href="" class="text-primary"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="" class="text-primary"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="" class="text-success"><i class="fa fa-whatsapp"></i></a></li>
+                        <li><a href="" class="text-info"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                </div>
+            </nav> 
+        </header>
